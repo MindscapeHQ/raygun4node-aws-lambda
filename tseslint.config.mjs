0e5523d72d01e4e57dfb37422b5e6bfcadb1a125
Miguel Beltran <m@beltran.work>
2024-09-24 10:29:39 +0200

    fixing ts and js eslint

--- tseslint.config.mjs
+++ tseslint.config.mjs
@@ -1,11 +1,19 @@
+// @ts-check
+
 import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
     // Basic eslint rules
     eslint.configs.recommended,
     // Codestyle rules for JS
     stylistic.configs["recommended-flat"],
+    // Codestyle rules for TS
+    stylisticTs.configs["all-flat"],
+    // Eslint rules for TS
+    ...tseslint.configs.recommended,
     {
         languageOptions: {
             // Add node globals to ignore undefined
@@ -20,14 +28,28 @@ export default [
             }
         },
         rules: {
+            // Unused vars reported as error
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    // Do not report unused function arguments
+                    "args": "none",
+                }
+            ],
             // Allow unused vars
-            "no-unused-vars": ["off", 0],
-            // Allow "exports"
-            "no-undef": ["off", 0],
+            "@typescript-eslint/no-unused-vars": ["off", 0],
+            // Allow the use of require for "debug()" and similar dependencies
+            "@typescript-eslint/no-require-imports": "off",
+            // Required to import JS modules
+            "@typescript-eslint/no-var-requires": "off",
             // Always require semicolons
             "@stylistic/semi": ["error", "always"],
             // Stick to double quotes
             "@stylistic/quotes": ["error", "double"],
+            // Always indent with two spaces
+            '@stylistic/ts/indent': ['error', 2],
+            // Enforce curly braces spacing
+            "@stylistic/ts/object-curly-spacing": ["error", "always"],
             // Enforce "one true brace style"
             "@stylistic/brace-style": ["error", "1tbs"],
             // Enforce parenthesis in functions: "(a) => a"
@@ -37,6 +59,10 @@ export default [
             "@stylistic/no-mixed-operators": ["off", 0],
             "@stylistic/operator-linebreak": ["off", 0],
             "@stylistic/quote-props": ["off", 0],
+            "@stylistic/ts/comma-dangle": ["off", 0],
+            "@stylistic/ts/no-extra-parens": ["off", 0],
+            "@stylistic/ts/quote-props": ["off", 0],
+            "@stylistic/ts/space-before-function-paren": ["off", 0],
         }
     }
-];
+);
